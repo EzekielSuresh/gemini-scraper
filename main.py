256b43491a34e4806dba9d5f005f96b2378afd5f
ezeksuresh03 <ezeksuresh03@gmail.com>
2025-09-11 02:04:11 +0800

    added call to concurrent_scraper

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from scraper.core_scraper import run_gemini_scraper
+from scraper.utils import filter_sub_urls
+from scraper.concurrent_scraper import scrap_suburls
  
 def main():
     url = input("Enter the URL to scrape: ").strip()
-    run_gemini_scraper(url)
-
+    scraped_page = run_gemini_scraper(url)
+    sub_urls = filter_sub_urls(scraped_page.sub_urls, scraped_page.url)
+    scrap_suburls(sub_urls, scraped_page.title)
+    print("Scraping complete!")    
+    
 if __name__ == "__main__":
     main()
