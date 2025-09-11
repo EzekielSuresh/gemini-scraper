351e1ecd20d338c8c53a69ae42a66cb7d9228d30
ezeksuresh03 <ezeksuresh03@gmail.com>
2025-09-11 18:44:01 +0800

    added logging function

--- scraper/utils.py
+++ scraper/utils.py
@@ -2,7 +2,7 @@ from urllib.parse import urlparse
 import re
 import json
 from pathlib import Path
-import os
+import logging
 
 #TODO: get sub_urls
 #TODO: parse and save result (page content)
@@ -38,3 +38,25 @@ def filter_sub_urls(sub_urls, base_url, limit=10):
     filtered = [url for url in sub_urls if url.startswith(base_url)]
     print(filtered[:limit])
     return filtered[:limit]
+
+def setup_logger(log_path='logs/scraper.log'):
+    Path(log_path).parent.mkdir(parents=True, exist_ok=True)
+    logger = logging.getLogger("scraper")
+    logger.setLevel(logging.INFO)
+    # Setup file handler
+    fh = logging.FileHandler(log_path, encoding='utf-8')
+    fh.setLevel(logging.INFO)
+    # Setup stream handler
+    sh = logging.StreamHandler()
+    sh.setLevel(logging.INFO)
+    # Format logs
+    formatter = logging.Formatter(
+        '[%(asctime)s] | %(levelname)s | %(filename)s:%(lineno)s | %(process)d >>> %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    fh.setFormatter(formatter)
+    sh.setFormatter(formatter)
+    # Add handler
+    logger.addHandler(fh)
+    logger.addHandler(sh)
+    
